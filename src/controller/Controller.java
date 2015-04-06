1dfcd2244442910eeb49236d97c9c5459475d8cc
prakol16 <prakol16@users.noreply.github.com>
2015-04-05 21:04:45 -0400

    Create Controller.java

---
+++ src/controller/Controller.java
@@ -0,0 +1,157 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import players.*;
+
+public class Controller {
+	private List<Card> discardPile;
+	private List<Player> players;
+	public Random rnd = new Random();
+	private int card;
+	private static final Class<?>[] playerClasses = {
+		Player1.class, Player2.class, Player3.class, Player4.class
+	};
+	public static void main(String[] args) {
+		Controller c = new Controller();
+		Map<Class<?>, Integer> scores = new HashMap<Class<?>, Integer>();
+		for (Class<?> playerClass : playerClasses) {
+			scores.put(playerClass, 0);
+		}
+		for (int i = 0; i < 1000; ++i) {
+			Player winner = c.playGame(false);
+			Class<?> winnerClass = winner.getClass();
+			scores.put(winnerClass, scores.get(winnerClass) + 1); 
+		}
+		System.out.println(scores);
+	}
+	private List<Class<?>> getPlayerClasses() {
+		List<Class<?>> playerClassList = Arrays.asList(playerClasses);
+		Collections.shuffle(playerClassList, rnd);
+		return playerClassList;
+	}
+	private Player playGame(boolean toLog) {
+		players = new ArrayList<>();
+		discardPile = new ArrayList<>();
+		for (Class<?> playerClass : getPlayerClasses()) {
+			try {
+				players.add((Player) playerClass.newInstance());
+			} catch (InstantiationException | IllegalAccessException e) {
+				e.printStackTrace();
+				return null;
+			}
+			if (players.size() == 4) break;
+		}
+		List<Card> deck = new ArrayList<Card>(52);
+		for (int i = 0; i < 13; ++i) {
+			for (int j = 0; j < 4; ++j) {
+				deck.add(new Card(i));
+			}
+		}
+		Collections.shuffle(deck, rnd);
+		for (int i = 0; i < 4; ++i) {
+			Player p = players.get(i);
+			p.drawCards(deck.subList(i * 13, (i + 1) * 13));
+		}
+		if (toLog) {
+			System.out.println(this);
+		}
+		for (int i = 0; i < 1000; ++i) {
+			runRound(players, toLog);
+			if (toLog) {
+				System.out.println(this);
+			}
+			Player winner = getWinner();
+			if (winner != null) {
+				if (toLog) {
+					System.out.println(winner + " won!");
+				}
+				return winner;
+			}
+		}
+		return null;
+	}
+	private Player getWinner() {
+		for (Player p : players) {
+			if (p.handSize() == 0) return p;
+		}
+		return null;
+	}
+	private void runRound(List<Player> players, boolean toLog) {
+		for (Player player : players) {
+			List<Card> cardsPlayed = player.requestCards(card, this);
+			if (cardsPlayed.size() == 0) throw new RuntimeException("Player " + player + " played zero cards");
+			boolean isBS = false;
+			for (Card c : cardsPlayed) {
+				if (c.getNumber() != card) isBS = true;
+				break;
+			}
+			
+			if (toLog) {
+				System.out.print(player + " played " + cardsPlayed.size() + " cards of " + card);
+				if (isBS) System.out.print(" (which are actually " + cardsPlayed + ")");
+				System.out.println();
+			}
+			
+			
+			player.removeCards(cardsPlayed);
+			discardPile.addAll(cardsPlayed);
+			List<Player> bs = new ArrayList<>();
+			for (Player p : players) {
+				if (p == player) continue;
+				if (p.bs(player, card, cardsPlayed.size(), this)) {
+					bs.add(p);
+				}
+			}
+			if (bs.size() != 0) {
+				Player playerThinksBS = bs.get(rnd.nextInt(bs.size()));
+				
+				if (toLog) {
+					System.out.println(playerThinksBS + " called BS");
+				}
+				
+				if (isBS) {
+					player.drawCards(discardPile);
+					discardPile.clear();
+					discardPile.add(playerThinksBS.removeRandomCard(rnd));
+				} else {
+					playerThinksBS.drawCards(discardPile);
+					discardPile.clear();
+				}
+			}
+			card++;
+			card %= 13;
+			if (getWinner() != null) {
+				break;
+			}
+		}
+	}
+
+	public int getDiscardPileSize() {
+		return discardPile.size();
+	}
+	public List<Player> getPlayers() {
+		return players;
+	}
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		for (Player p : players) {
+			str.append(p);
+			str.append(": ");
+			str.append(p.handAsString());
+			str.append(" " + p.handSize() + " cards");
+			str.append('\n');
+		}
+		str.append("Discard pile: ");
+		str.append(discardPile);
+		str.append(" " + discardPile.size() + " cards");
+		str.append('\n');
+		return str.toString();
+	}
+}
